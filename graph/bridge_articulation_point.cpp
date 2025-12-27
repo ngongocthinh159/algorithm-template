50d8af592e6041454f2bb7088f9c4fc5cf260bb6
ngongocthinh124 <ngongocthinh124@gmail.com>
2025-12-27 12:36:43 +0700

    Update bridge_articulation_point.cpp

--- graph/bridge_articulation_point.cpp
+++ graph/bridge_articulation_point.cpp
@@ -1,15 +1,16 @@
 
 // undirected graph
+// Tarjan
 int color[N], low[N], num[N];
 int timer = 0;
 vector<int> g[N];
-void dfs2(int u, int p) {
+void dfs(int u, int p) {
     color[u] = 1;
     low[u] = num[u] = timer++;
     int child = 0;
     for (auto v : g[u]) if (v != p) {
         if (color[v] == 0) { // span edge
-            dfs2(v, u);
+            dfs(v, u);
             low[u] = min(low[u], low[v]);
             if (num[u] < low[v])
                 IS_BRIDGE(u, v)
@@ -20,7 +21,42 @@ void dfs2(int u, int p) {
             low[u] = min(low[u], num[v]);
         }
     }
-    if (p == -1 && child > 1)
+    if (p == -1 && child > 1) // special case root node
         IS_ARTICULATION_PONT(u);
+    color[u] = 2;
+}
+
+
+
+// undirected graph
+// Back edge cross over method
+// https://codeforces.com/blog/entry/68138
+// dp[u]: number of back edge "cross over" the span edge connect u and parent of u
+// if dp[u] == 0 => no back edge cross over => span edge connect u and parent of u is a bridge
+int color[N], up[N], down[N];
+void dfs(int u, int p) {
+    color[u] = 1;
+    int sumc = 0, child = 0;
+    for (auto v : g[u]) if (v != p) {
+        if (color[v] == 0) {
+            dfs(v, u);
+            child++;
+            sumc += dp[v];
+            if (dp[v] == 0)
+                IS_BRIDGE(u -> v)
+            if (dp[v])
+        } else if (color[v] == 1) {
+            down[v]++;
+            up[v]++;
+        }
+    }
+    dp[u] = up[u] - down[u] + sumc;
+    if (p == -1) {
+        if (child > 1) IS_ARTICULATION_POINT(u);
+    } else {
+        // actually dp[u] - back edges from u -> par u
+        // but in undirected graph, we do not have those edges
+        if (dp[u] == 0) IS_ARTICULATION_POINT(u);
+    }
     color[u] = 2;
 }
